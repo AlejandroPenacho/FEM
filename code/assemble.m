e37a84f8401e743d4e02eb98fc6466a30d5855a1
Alejandro <alejpr@kth.se>
2020-11-03 14:07:39 +0100

    assemble.m function has been modified in order to assemble the stiffness matrix

--- code/assemble.m
+++ code/assemble.m
@@ -1,37 +1,68 @@
 function [K,Q,M,Ksigma]=assemble(le,EI,GJ,I0,A,J0,q,qt,S,T,m,P,ndof,nelem);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Assemble system stiffness matrix, load vector, mass matrix (not necessary)
-% and initial stress matrix
-% File name: assemble.m
-%
-% K		System stiffness matrix
-% Q		System load vector
-% M		System mass matrix
-% Ksigma       System initial stress matrix
-%
-% le		element length [m]
-% EI		element bending stiffness [Nm2]
-% GJ		element torsional stiffness [Nm2]
-% I0		element polar moment of inertia [m4]
-% A		element cross-section area [m2]
-% J0		element mass moment of inertia [kgm]
-% q		element transverse pressure load [N/m]
-% qt		element torsion pressure load [Nm/m]
-% S		transverse point load [N]
-% T		local torque [Nm]
-% m		element mass per unit length [kg/m]
-% P		applied buckling load [N], positive if tensile
-% ndof		number of degrees of freedom
-% nelem		number of elements
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Assemble system stiffness matrix, load vector, mass matrix (not necessary)
+    % and initial stress matrix
+    % File name: assemble.m
+    %
+    % K		System stiffness matrix
+    % Q		System load vector
+    % M		System mass matrix
+    % Ksigma       System initial stress matrix
+    %
+    % le		element length [m]
+    % EI		element bending stiffness [Nm2]
+    % GJ		element torsional stiffness [Nm2]
+    % I0		element polar moment of inertia [m4]
+    % A		element cross-section area [m2]
+    % J0		element mass moment of inertia [kgm]
+    % q		element transverse pressure load [N/m]
+    % qt		element torsion pressure load [Nm/m]
+    % S		transverse point load [N]
+    % T		local torque [Nm]
+    % m		element mass per unit length [kg/m]
+    % P		applied buckling load [N], positive if tensile
+    % ndof		number of degrees of freedom
+    % nelem		number of elements
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-K=zeros(ndof);
-Q=zeros(ndof,1);
-M=zeros(ndof);
-Ksigma=zeros(ndof);
+    K=zeros(ndof);
+    Q=zeros(ndof,1);
+    M=zeros(ndof);
+    Ksigma=zeros(ndof);
+    
+    %% Assemble of stiffness matrix
+    
+    % For every element, the element stiffness matrix is computed and added
+    % to the total stiffness matrix of the structure
+    for iElement = 1:nelem
+        
+        % For the case of a simple beam, the nodes at the tips of each
+        % element are easily computed. Nodes are explicitly calculated so
+        % changes can be done easily to this process.
+        
+        firstNode = iElement;
+        secondNode = iElement + 1;
+        
+        
+        firstNodeBaseIndex = (firstNode-1) * 3;
+        secondNodeBaseIndex = (secondNode-1) * 3;
+        
+        [Ke]=elk(le,EI,GJ);
+        
+        deltaK = zeros(ndof);
+        
+        for i=1:6
+            for j=1:6
+                deltaK(firstNodeBaseIndex +i-1, secondNodeBaseIndex +j-1) = Ke(i,j);
+            end
+        end
+    end
 
 
+
+
+end
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
