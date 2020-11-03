019ed672571123c6d931e60e72d96e1845aaadd6
sankhaffaf <sankhaffaf@n148-p108.eduroam.kth.se>
2020-11-03 15:59:04 +0100

    Changed another error of using the wrong variable name.

--- code/elksigma.m
+++ code/elksigma.m
@@ -19,8 +19,8 @@ Kesigma = zeros(6,6);
 Kesigma(1,1:6) = (P/(30*le))*[36, -3*le, 0, -36, -3*le, 0];
 Kesigma(2,2:6) = (P/(30*le))*[4*le^2, 0, 3*le, -le^2, 0];
 Kesigma(3,3:6) = (I0*P/(A*le))*[1, 0, 0, -1];
-Kesigma(4,4:6) = (P/(30*le))*[36, 3*h, 0];
-Kesigma(5,5:6) = (P/(30*le))*[4*h^2, 0];
+Kesigma(4,4:6) = (P/(30*le))*[36, 3*le, 0];
+Kesigma(5,5:6) = (P/(30*le))*[4*le^2, 0];
 Kesigma(6,6)   = (I0*P/(A*le));
 
 %Making it symmetric
